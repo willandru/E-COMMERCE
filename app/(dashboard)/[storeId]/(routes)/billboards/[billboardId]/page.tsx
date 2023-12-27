92d32fbc99459a4be1bcc64507fbffa904ac9bd9
willandru <willandru@hotmail.com>
2023-12-27 00:32:44 -0500

    DAshboarding

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({
     params
@@ -13,7 +14,12 @@ const BillboardPage = async ({
     });
 
     return(
-        <div>Existing Billboard: {billboard?.label} </div>
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm 
+                initialData={billboard}/>
+            </div>
+        </div>
     );
 }
 
